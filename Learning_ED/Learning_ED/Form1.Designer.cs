34398807a2226904d2a934ab91f63c76acbd3e22
Nazar <38031744+NazarPelypets1@users.noreply.github.com>
2020-07-11 11:58:09 +0300

    Add files via upload

---
+++ Learning_ED/Learning_ED/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Learning_ED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button_Close = new System.Windows.Forms.Button();
+            this.button_Run = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_N = new System.Windows.Forms.TextBox();
+            this.textBox__n = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(597, 78);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 138);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(597, 78);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button_Close
+            // 
+            this.button_Close.Location = new System.Drawing.Point(533, 263);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(75, 23);
+            this.button_Close.TabIndex = 2;
+            this.button_Close.Text = "Close";
+            this.button_Close.UseVisualStyleBackColor = true;
+            this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // button_Run
+            // 
+            this.button_Run.Location = new System.Drawing.Point(452, 263);
+            this.button_Run.Name = "button_Run";
+            this.button_Run.Size = new System.Drawing.Size(75, 23);
+            this.button_Run.TabIndex = 3;
+            this.button_Run.Text = "Run";
+            this.button_Run.UseVisualStyleBackColor = true;
+            this.button_Run.Click += new System.EventHandler(this.button_Run_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Код ASCII тексту";
+            // 
+            // textBox_N
+            // 
+            this.textBox_N.Location = new System.Drawing.Point(230, 222);
+            this.textBox_N.Name = "textBox_N";
+            this.textBox_N.Size = new System.Drawing.Size(100, 20);
+            this.textBox_N.TabIndex = 6;
+            this.textBox_N.Text = "4";
+            // 
+            // textBox__n
+            // 
+            this.textBox__n.Location = new System.Drawing.Point(230, 248);
+            this.textBox__n.Name = "textBox__n";
+            this.textBox__n.Size = new System.Drawing.Size(100, 20);
+            this.textBox__n.TabIndex = 7;
+            this.textBox__n.Text = "4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Кількість входів нейронної мережі (N)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Розрядність входів нейронної мережі (n)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 298);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox__n);
+            this.Controls.Add(this.textBox_N);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Run);
+            this.Controls.Add(this.button_Close);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button_Close;
+        private System.Windows.Forms.Button button_Run;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_N;
+        private System.Windows.Forms.TextBox textBox__n;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Learning_ED/Learning_ED/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Learning_ED
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        string[] bin_ASCII = {"01100001",
+"01100010",
+"01100011",
+"01100100",
+"01100101",
+"01100110",
+"01100111",
+"01101000",
+"01101001",
+"01101010",
+"01101011",
+"01101100",
+"01101101",
+"01101110",
+"01101111",
+"01110000",
+"01110001",
+"01110010",
+"01110011",
+"01110100",
+"01110101",
+"01110110",
+"01110111",
+"01111000",
+"01111001",
+"01111010",
+"01000001",
+"01000010",
+"01000011",
+"01000100",
+"01000101",
+"01000110",
+"01000111",
+"01001000",
+"01001001",
+"01001010",
+"01001011",
+"01001100",
+"01001101",
+"01001110",
+"01001111",
+"01010000",
+"01010001",
+"01010010",
+"01010011",
+"01010100",
+"01010101",
+"01010110",
+"01010111",
+"01011000",
+"01011001",
+"01011010",
+"00110001",
+"00110010",
+"00110011",
+"00110100",
+"00110101",
+"00110110",
+"00110111",
+"00111000",
+"00111001",
+"00110000",
+"00101110",
+"00101100",
+"00100000",
+"00100001",
+"00111111",
+"00101101",
+"00101000",
+"00101001",};
+
+        string char_ASCII = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890., !?-()";
+
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button_Run_Click(object sender, EventArgs e)
+        {
+            string s = richTextBox1.Text;
+            Encoding ascii = Encoding.ASCII;
+            string s_ascii = "";
+            Byte[] encodedBytes = ascii.GetBytes(s);
+            richTextBox2.Text = "";
+            bool flag = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                flag = true;
+                for (int j = 0; j < char_ASCII.Length; j++)
+                {
+                    if (s[i].Equals(char_ASCII[j]))
+                    {
+                        richTextBox2.Text += bin_ASCII[j] + "\n";
+                        s_ascii += bin_ASCII[j];
+
+                        flag = false;
+                        break;
+                    }
+
+                }
+                if (flag)
+                {
+                    MessageBox.Show("Символ '" + s[i] + "' не найдкно в таблиці!", "Error", MessageBoxButtons.OK);
+                    richTextBox2.Text += "Символ '" + s[i] + "' не найдкно в таблиці!\n";
+
+                }
+            }
+
+
+            StreamWriter sw = new StreamWriter("навчаючі пари.txt");
+            int n = Convert.ToInt32(textBox__n.Text);
+            int N = Convert.ToInt32(textBox_N.Text);
+            while (true)
+            {
+                if (s_ascii.Length / (1.0 * n * N) == Math.Round(s_ascii.Length / (1.0 * n * N))) break;
+                s_ascii += "0";
+            }
+            sw.WriteLine("Кількість навчаючих пар: " + (s_ascii.Length / (1.0 * n * N)));
+            string temps = "";
+            for (int i = 0; i < s_ascii.Length; )
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    temps = "x" + j + " = ";
+                    for (int k = 0; k < n; k++, i++)
+                        temps += s_ascii[i];
+                    sw.WriteLine(temps);
+                }
+
+
+
+            }
+            sw.Close();
+
+        }
+    }
+}
